f1a9eb3010908cfc1c02abefe701abe996e808e0
hitsub2 <yangdongdong@sursen.net>
2017-03-01 20:53:04 -0500

    fix role

--- initcloud_web/biz/role/models.py
+++ initcloud_web/biz/role/models.py
@@ -12,17 +12,3 @@ class Role(models.Model):
     datacenter = models.IntegerField(_("Result"), default=0, null=False)
     deleted = models.BooleanField(_("Deleted"), default=False)
     create_date = models.DateTimeField(_("Create Date"), auto_now_add=True)
-
-    @classmethod
-    def log(cls, obj, obj_name, action, result=1, udc=None, user=None):
-
-        try:
-            Role.objects.create(
-                resource=obj.__class__.__name__,
-                resource_id=obj.id,
-                resource_name=obj_name,
-                action=action,
-                result=result
-            )
-        except Exception as e:
-            pass
